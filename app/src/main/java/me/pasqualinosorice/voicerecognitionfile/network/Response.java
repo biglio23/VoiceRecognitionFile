8fba83d4e732febce9c1b0aab91442518ef9e63d
Pasqualino <lino.sorice@gmail.com>
2015-07-04 08:07:10 +0200

    bugfix

--- app/src/main/java/me/pasqualinosorice/voicerecognitionfile/network/Response.java
+++ app/src/main/java/me/pasqualinosorice/voicerecognitionfile/network/Response.java
@@ -6,11 +6,13 @@ public class Response {
     private List<Result> result;
 
     public String getFirstTranscription() {
-        return result.get(0).getAlternative().get(0).getTranscript();
+        if (result != null && result.size() > 0 && result.get(0) != null)
+            return result.get(0).getAlternative().get(0).getTranscript();
+        return "";
     }
 
     public double getConfidence() {
-        if (result.get(0).getAlternative().get(0).getConfidence() != null) {
+        if (result != null && result.size() > 0 && result.get(0) != null && result.get(0).getAlternative().get(0).getConfidence() != null) {
             return Double.parseDouble(result.get(0).getAlternative().get(0).getConfidence());
         } else {
             return 1;
